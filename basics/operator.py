9286fd257ecbad6afce818cf39406c4a9952da70
Lucas Dousseau Arantes <53654475+xoLucas@users.noreply.github.com>
2025-10-29 11:04:54 -0300

    Create operator.py

---
+++ basics/operator.py
@@ -0,0 +1,32 @@
+#in this section, I will explain about the operators that i'll use in my codes
+
+#Addition -> a + b
+#Subtraction -> a - b
+#Multiplication -> a * b
+#Division -> a / b
+#Exponenciation -> a ** b
+#Module -> a % b
+#int part of division -> a // b
+
+#+= is an operator that make this operation: a = a + b => a += b , you can replace + with any other operator mentioned bellow
+
+#comparasion operators:
+#returns a true of false value about a comparasion of two numbers
+#Equal -> a == b
+#Greater -> a > b
+#Less -> a < b
+#Not -> a != b
+#Greater or equal -> a >= b
+#Less or equal -> a <= b
+
+#Logic operator also return true or false values, but bettwen boolean values:
+#and -> a and b
+#or -> a or b
+#not -> not(a)
+
+#Bitwise operator return a number before made a logic comperation of bits of a number:
+#For exemple, look: a = 1 & 0 => a = 01 & 00 = 00 (making a logic comparation of all bits of numbers) or, another exemple, b = 2 & 1 = 10 & 01 = 00
+#& -> a & b (compare all bits of a and b and return a number (in binary))
+#| -> a | b (or)
+#^ -> a ^ b (XOR)
+#~ -> ~a (NOT)
