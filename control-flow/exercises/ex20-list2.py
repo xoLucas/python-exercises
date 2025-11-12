cf978600ed5fea1bdc0676dcc4719825421ca601
Lucas Dousseau Arantes <53654475+xoLucas@users.noreply.github.com>
2025-11-12 10:52:49 -0300

    Fix ex20-list2

--- control-flow/exercises/ex20-list2.py
+++ control-flow/exercises/ex20-list2.py
@@ -3,14 +3,20 @@
 def triangulo (a, b, c):
     return a + b > c and a + c > b and b + c > a
 
+def pitagoras (a, b, c):
+    return (a**2 + b**2)**(1/2) == c or (a**2 + c**2)**(1/2) == b or (c**2 + b**2)**(1/2) == a
+
+def dif (a, b, c):
+    return (a != b and b != c and a != c)
+
 lado_a = int(input("\nDiga o tamanho de um lado a: "))
 lado_b = int(input("Diga o tamanho de um lado b: "))
 lado_c = int(input("Diga o tamanho de um lado c: "))
 
 if (triangulo (lado_a, lado_b, lado_c)):
-    if ((lado_a**2 + lado_b**2)**(1/2) == lado_c):
+    if (pitagoras (lado_a, lado_b, lado_c)):
         print("O triangulo é retângulo!")
-    elif (lado_a != lado_b and lado_b != lado_c and lado_a != lado_c):
+    elif (dif (lado_a, lado_b, lado_c)):
         print("O triangulo é escaleno!")
     else:
         print("O triangulo é equilátero!")
