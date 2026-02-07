6f1fca7ac268972e870e29fc017ee17793ff080c
Lucas Dousseau Arantes <53654475+xoLucas@users.noreply.github.com>
2026-02-07 09:06:27 -0300

    Update ex1-list5.py

--- lists/exercises/ex1-list5.py
+++ lists/exercises/ex1-list5.py
@@ -1,1 +1,26 @@
 #Faça um algoritmo que leia um lista de números V[6]. Contar a seguir, quantos valores de V são negativos e mostre essa informação.
+
+
+def pede_lista ():
+    V = []
+    while len(V) < 6:
+        n = int(input("Digite um número: "))
+        V.append(n)
+    return V
+
+def conta_negativo (L):
+    c_n = 0
+    for i in L:
+    
+        if i < 0 :
+            c_n += 1
+    return c_n
+
+
+
+
+def main ():
+    
+    print(f"Existem {conta_negativo(pede_lista())} núumeros negativos na lista")
+
+main()
